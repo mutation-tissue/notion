3ecdb5433a5728e84638557e9569dd3b3fc29fad
mutation-tissue <katotai2001@gmail.com>
2024-12-22 10:24:05 +0900

    set env key

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("hello wddorld")
+import requests
+import os
+
+# 環境変数を取得
+API_KEY = os.getenv("NOTION_API_KEY")
+PARENT_PAGE_ID = os.getenv("PARENT_PAGE_ID")
+
+print(f"API_KEY: {API_KEY}")
+print(f"PARENT_PAGE_ID: {PARENT_PAGE_ID}")
+
